433f332cebd6a9177c361fd7712c28d5726fa90b
Lau Siu Hin Marco <marcolau733@gmail.com>
2025-05-14 13:04:38 +0800

    [NEW] Contact customization for data importing

---
+++ recreate_HangHeung/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Hang Heung Customizations',
+    'version': '1.0',
+    'summary': 'Includes all Hang Heung customizations',
+    'description': """
+        This module contains all of Hang Heung customizations.
+    """,
+    'author': 'Lau Siu Hin',
+    'website': '',
+    'depends': ['contacts', 'base', 'stock', 'web'],
+    'data': [
+        "views/res_partner.xml",
+        "views/customer_category_data.xml",
+        "security/ir.model.access.csv",
+    ],
+
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ recreate_HangHeung/models/res_partner.py
@@ -0,0 +1,25 @@
+from odoo import models, fields, api, _
+from datetime import datetime
+import logging
+from odoo.exceptions import ValidationError
+import re
+
+_logger = logging.getLogger(__name__)
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    customer = fields.Boolean(string='Customer', default=False, stored=True)
+    vendor = fields.Boolean(string='Vendor', default=False, stored=True)
+    contact_number = fields.Char(string='Contact No.', stored=True)
+    alternative_contact_name = fields.Char(string='Alternative Name', stored=True)
+    fax_number = fields.Char(string='Fax No.', stored=True)
+    customer_credit_limit = fields.Integer(string='Credit Limit', default=0, stored=True)
+    crdr_due_days = fields.Integer(string='CRDR Due Days', default=0, stored=True)
+    customer_category_id = fields.Many2one(
+        'customer.category', 
+        string='Customer Category'
+    )
+    terms = fields.Char(string='Terms', stored=True)
+   
