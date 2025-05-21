7f79a7a29d91c29150dbf539db12f0af1b580a88
andrew7202 <andrew.xie7202@gmail.com>
2025-05-22 00:23:56 +0800

    [ADD]replace_date_to_datetime

---
+++ replace_date_to_datetime/static/src/js/pos_store.js
@@ -0,0 +1,25 @@
+import {patch} from "@web/core/utils/patch";
+import {PosStore} from "@point_of_sale/app/store/pos_store";
+const {DateTime} = luxon;
+
+patch(PosStore.prototype, {
+    async processServerData() {
+        await super.processServerData();
+        for (const program of this.models["loyalty.program"].getAll()) {
+            // console.log(program, 1111111)
+            if (program.date_to) {
+                const parts = program.date_to.invalid.explanation.split('"');
+                const extractedDate = parts.length >= 2 ? parts[1] : null;
+
+                program.date_to = DateTime.fromFormat(extractedDate, "yyyy-MM-dd HH:mm:ss");
+                program.date_to = DateTime.fromISO(program.date_to);
+            }
+            if (program.date_from) {
+                const parts1 = program.date_from.invalid.explanation.split('"');
+                const extractedDate1 = parts1.length >= 2 ? parts1[1] : null;
+                program.date_from = DateTime.fromFormat(extractedDate1, "yyyy-MM-dd HH:mm:ss");
+                program.date_from = DateTime.fromISO(program.date_from);
+            }
+        }
+    },
+});
