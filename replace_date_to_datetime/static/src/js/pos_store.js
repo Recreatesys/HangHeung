42345856243365e7375941980939fe592495751b
andrew7202 <andrew.xie7202@gmail.com>
2025-05-22 11:25:33 +0800

    [FIX]Time zone error

--- replace_date_to_datetime/static/src/js/pos_store.js
+++ replace_date_to_datetime/static/src/js/pos_store.js
@@ -10,14 +10,13 @@ patch(PosStore.prototype, {
             if (program.date_to) {
                 const parts = program.date_to.invalid.explanation.split('"');
                 const extractedDate = parts.length >= 2 ? parts[1] : null;
-
-                program.date_to = DateTime.fromFormat(extractedDate, "yyyy-MM-dd HH:mm:ss");
+                program.date_to = DateTime.fromFormat(extractedDate, "yyyy-MM-dd HH:mm:ss", {zone: "utc"});
                 program.date_to = DateTime.fromISO(program.date_to);
             }
             if (program.date_from) {
                 const parts1 = program.date_from.invalid.explanation.split('"');
                 const extractedDate1 = parts1.length >= 2 ? parts1[1] : null;
-                program.date_from = DateTime.fromFormat(extractedDate1, "yyyy-MM-dd HH:mm:ss");
+                program.date_from = DateTime.fromFormat(extractedDate1, "yyyy-MM-dd HH:mm:ss", {zone: "utc"});
                 program.date_from = DateTime.fromISO(program.date_from);
             }
         }
