d44df464bc1e0de16914aa934bd7aa9089ffb568
andrew7202 <andrew.xie7202@gmail.com>
2025-07-09 01:10:16 +0800

    [IMP]Change POS receipt3

--- custom_pos_receipt/static/src/js/models.js
+++ custom_pos_receipt/static/src/js/models.js
@@ -5,12 +5,23 @@ import { PosStore } from "@point_of_sale/app/store/pos_store";
 
 patch(PosStore.prototype, {
     getReceiptHeaderData(order) {
+        const totalQty = order.lines.reduce(
+            (sum, line) => {
+                if (!line.is_reward_line && line.product_id.type !== "service" &&
+                (!line.combo_line_ids || line.combo_line_ids.length === 0)) {
+                    return sum + (parseFloat(line.qty) || 0);
+                }
+                return sum;
+            },
+            0
+        );
         return {
             ...super.getReceiptHeaderData(...arguments),
             partner: order.partner_id,
             orderRef: order.name,
             storeName: order.config_id.name,
-            storeCashier:order.cashier
+            storeCashier:order.cashier,
+            totalQty: totalQty
         };
     },
 });
