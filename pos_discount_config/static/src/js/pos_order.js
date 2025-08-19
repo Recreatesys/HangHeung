ec6a5ab52cd94ba1827ab2608584cac1c28c5f85
Chand Prakash Rawat <chandprakash@gmail.com>
2025-08-19 10:56:53 +0530

    [Fix]Fix the exclued product discount issue.

--- pos_discount_config/static/src/js/pos_order.js
+++ pos_discount_config/static/src/js/pos_order.js
@@ -3,6 +3,10 @@
 import { patch } from "@web/core/utils/patch";
 import { PosOrder } from "@point_of_sale/app/models/pos_order";
 
+function _newRandomRewardCode() {
+    return (Math.random() + 1).toString(36).substring(3);
+}
+
 patch(PosOrder.prototype, {
     /**
      * Override _getDiscountableOnSpecific to customize discount logic.
@@ -187,7 +191,101 @@ patch(PosOrder.prototype, {
         }
 
         return { discountable, discountablePerTax };
-    }
+    },
 
+    _getRewardLineValuesDiscount(args) {
+
+        const reward = args["reward"];
+        const coupon_id = args["coupon_id"];
+        const rewardAppliesTo = reward.discount_applicability;
+        let getDiscountable;
+        if (rewardAppliesTo === "order") {
+            getDiscountable = this._getDiscountableOnOrder.bind(this);
+        } else if (rewardAppliesTo === "cheapest") {
+            getDiscountable = this._getDiscountableOnCheapest.bind(this);
+        } else if (rewardAppliesTo === "specific") {
+            getDiscountable = this._getDiscountableOnSpecific.bind(this);
+        }
+        if (!getDiscountable) {
+            return _t("Unknown discount type");
+        }
+        let { discountable, discountablePerTax } = getDiscountable(reward);
+        discountable = Math.min(this.get_total_with_tax(), discountable);
+        if (!discountable) {
+            return [];
+        }
+        let maxDiscount = reward.discount_max_amount || Infinity;
+        if (reward.discount_mode === "per_point") {
+            const points = ["ewallet", "gift_card"].includes(reward.program_id.program_type)
+                ? this._getRealCouponPoints(coupon_id)
+                : Math.floor(this._getRealCouponPoints(coupon_id) / reward.required_points) *
+                  reward.required_points;
+            maxDiscount = Math.min(maxDiscount, reward.discount * points);
+        } else if (reward.discount_mode === "per_order") {
+            maxDiscount = Math.min(maxDiscount, reward.discount);
+        } else if (reward.discount_mode === "percent") {
+            maxDiscount = Math.min(maxDiscount, discountable * (reward.discount / 100));
+        }
+        const rewardCode = _newRandomRewardCode();
+        let pointCost = reward.clear_wallet
+            ? this._getRealCouponPoints(coupon_id)
+            : reward.required_points;
+        if (reward.discount_mode === "per_point" && !reward.clear_wallet) {
+            pointCost = Math.min(maxDiscount, discountable) / reward.discount;
+        }
+
+        const discountProduct = reward.discount_line_product_id;
+        if (["ewallet", "gift_card"].includes(reward.program_id.program_type)) {
+            const new_price = compute_price_force_price_include(
+                discountProduct.taxes_id,
+                -Math.min(maxDiscount, discountable),
+                discountProduct,
+                this.config._product_default_values,
+                this.company,
+                this.currency,
+                this.models
+            );
+
+            return [
+                {
+                    product_id: discountProduct,
+                    price_unit: new_price,
+                    qty: 1,
+                    reward_id: reward,
+                    is_reward_line: true,
+                    coupon_id: coupon_id,
+                    points_cost: pointCost,
+                    reward_identifier_code: rewardCode,
+                    tax_ids: discountProduct.taxes_id,
+                },
+            ];
+        }
+        const discountFactor = discountable ? Math.min(1, maxDiscount / discountable) : 1;
+        const result = Object.entries(discountablePerTax).reduce((lst, entry) => {
+
+            if (!entry[1]) {
+                return lst;
+            }
+            let taxIds = entry[0] === "" ? [] : entry[0].split(",").map((str) => parseInt(str));
+            taxIds = this.models["account.tax"].filter((tax) => taxIds.includes(tax.id));
+
+            lst.push({
+                product_id: discountProduct,
+                price_unit: -(entry[1] * discountFactor),
+                qty: 1,
+                reward_id: reward,
+                is_reward_line: true,
+                coupon_id: coupon_id,
+                points_cost: 0,
+                reward_identifier_code: rewardCode,
+                tax_ids: taxIds,
+            });
+            return lst;
+        }, []);
+        if (result.length) {
+            result[0]["points_cost"] = pointCost;
+        }
+        return result;
+    },
 
 });
